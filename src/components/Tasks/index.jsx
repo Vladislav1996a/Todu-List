8af2179bebf6eed063b54f68e7451b37ca546908
Vlad Krystal <vladyslav.krystal@studio-luck.ru>
2021-07-25 00:57:48 +0300

    Updata

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -17,7 +17,13 @@ function Tasks({list,changeTitle,addTasks}) {
         })
       }
    }
+   const taskComplite = (id,completed) => {
+        axios.patch('http://localhost:3001/tasks/' + id,{
+            completed: !completed
+        })
+   }
   
+ 
     return (
         <div className='task'>
             <h2 className='task__title' style={{color:list.color.hex}}>
@@ -31,7 +37,7 @@ function Tasks({list,changeTitle,addTasks}) {
                     
                     <div key={item.id} className="checkbox">
                       
-                        <input id={`check_${item.id}`} type="checkbox"/>
+                        <input  onChange={()=>taskComplite(item.id,item.completed)} defaultChecked={item.completed}  id={`check_${item.id}`} type="checkbox"/>
                         <label htmlFor={`check_${item.id}`}>
                             <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
